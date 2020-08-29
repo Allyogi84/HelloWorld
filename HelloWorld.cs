c2793bac766d02fe1892b775f4f2a715249cd141
Anthony Allgood <62567954+Allyogi84@users.noreply.github.com>
2020-08-29 16:33:17 -0400

    new change

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GettingInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            //this is a new version of the code that I wrote//
+            string input = Console.Readline();
+            Console.Writeline($"You are {input} years old!")
+        }
+    }
+} 
+
+
